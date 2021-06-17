08d304940fbf1579e05fc496f9624769c83f7481
Mahmoud Alewiwi <alewiwi@sabanciuniv.edu>
2021-06-17 19:21:18 +0300

    Serializers Done

--- api/models.py
+++ api/models.py
@@ -8,14 +8,22 @@ class TaskUser(models.Model):
     isadmin = models.BooleanField(default=False)
     name = models.CharField(max_length=20)
 
+    def __str__(self):
+        return self.name
+
 class Task(models.Model):
     title = models.CharField(max_length=20)
     details = models.TextField()
     date = models.DateField()
-    tasks = models.ForeignKey(TaskUser,on_delete=models.CASCADE)
+    user = models.ForeignKey(TaskUser,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
 
 class Attachement(models.Model):
     title = models.CharField(max_length=20)
     task = models.ForeignKey(Task,on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.title
     
--- api/serializers.py
+++ api/serializers.py
@@ -1,16 +1,15 @@
 from rest_framework import serializers
 from .models import Task,TaskUser,Attachement
 
+
+class TaskUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TaskUser
+        fields = '__all__'
+
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
-        fields = '__all__'
-
-
-class TaskUserSerializer(serializers.ModelSerializer):
-    tasks = TaskSerializer(many=True)
-    class Meta:
-        model = TaskUser
         fields = '__all__'
 
 class AttachmentSerializer(serializers.ModelSerializer):
